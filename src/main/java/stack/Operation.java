96603c3462e0fdb1d2211355351598f1a26b9c06
treshane-ranasinghe <treshaneranasinghe@outlook.com>
2025-08-30 22:50:29 +0530

    created stack

---
+++ src/main/java/stack/Operation.java
@@ -0,0 +1,26 @@
+package stack;
+
+import org.example.Post;
+
+public class Operation {
+    public enum Type { ADD, REMOVE, UPDATE }
+
+    public Type type;
+    public Post post;
+    public int oldLikes;
+    public int newLikes;
+
+    // For ADD/REMOVE
+    public Operation(Type type, Post post) {
+        this.type = type;
+        this.post = post;
+    }
+
+    // For UPDATE
+    public Operation(Type type, Post post, int oldLikes, int newLikes) {
+        this.type = type;
+        this.post = post;
+        this.oldLikes = oldLikes;
+        this.newLikes = newLikes;
+    }
+}
